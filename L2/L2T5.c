996980f427260364d0462bf92d38f7fc6f281e2e
Erkki Pokkinen <lnxbusdrv@gmail.com>
2023-06-03 18:30:29 +0300

    100% Vinkki:Alusta muuttujat

--- L2/L2T5.c
+++ L2/L2T5.c
@@ -1,117 +1,109 @@
-/* un 20230530 L2T5.c */
-/******************************************************************/
-#include <stdio.h>
+    /* lnxsbusdrvr 20230603 L2T5.c */
+    /******************************************************************/
+    #include <stdio.h>
 
-#define MAX 200
-#define FALSE 0
-#define TRUE 1
+    #define MAX 200
+    #define FALSE 0
+    #define TRUE 1
 
-int annaKoko(char *str);
+    int annaKoko(char *str);
 
-int main(void) {
-  int nValinta;
+    int main(void) {
+      int nValinta = 0;
 
-  char sMjono[MAX];
-  int iRivi = 0;
+      /* HUOM
+       * Alusta kaikki muuttujat niin ei tulostu
+       * merkkijonoissa roskia 
+       */
+      char sMjono[MAX] = {' '};
+      int iRivi = 0;
 
-  int bOnkoMjonossaTietuetta = FALSE;
+      int bOnkoMjonossaTietuetta = FALSE;
 
-  /* Merkkijonopointteri 
-   * merkkien järjestyksen 
-   * tallentamiseen 
-   * Oikeastaan koon selvittämiseen
-   */
-  char *pMjono;
+      int mjonokoko = 0;
 
-  int mjonokoko;
+      printf("\n");
+      do {
+        printf("Valikko\n");
+        printf("1) Lisää uusi merkki\n");
+        printf("2) Tyhjennä merkkijono\n");
+        printf("3) Tulosta merkkijono\n");
+        printf("0) Lopeta\n");
+        printf("Valintasi: ");
+        
+        scanf("%d", &nValinta); 
 
-  printf("\n");
-  do {
-    printf("Valikko\n");
-    printf("1) Lisää uusi merkki\n");
-    printf("2) Tyhjennä merkkijono\n");
-    printf("3) Tulosta merkkijono\n");
-    printf("0) Lopeta\n");
-    printf("Valintasi: ");
-    
-    scanf("%d", &nValinta); 
+        mjonokoko = annaKoko(sMjono);
 
-    pMjono = sMjono;
-    mjonokoko = annaKoko(pMjono);
+        switch (nValinta) {
+          case 0:
+            nValinta = 0;
+            break;
 
-    switch (nValinta) {
-      case 0:
-        nValinta = 0;
-        break;
+          case 1:
+            printf("\n");
+            printf("Anna jokin merkki: ");
+            getchar();
+            if (scanf(" %c", &sMjono[iRivi]) == '\n') {
+              printf("\nMerkkijono oli tyhjä.");
+            } else if(annaKoko(sMjono) > MAX) {
+              printf("\nMerkkijonoon ei mahdu enempää merkkejä.");
+            } else {
+              bOnkoMjonossaTietuetta = TRUE; 
+              iRivi++;
+            }
 
-      case 1:
-        printf("\n");
-        printf("Anna jokin merkki: ");
-        scanf("%s", &sMjono[iRivi]);
-        /*if (sMjono[iRivi] == '\n' && sMjono[iRivi+1] == '\0') {*/
-        /**if(getchar() == '\n' && getchar() == EOF) { */
-        if (getchar() == '\n' && sMjono[iRivi] == '\0') {
-          printf("\nMerkkijono oli tyhjä.");
-        } else if(sMjono[iRivi] > MAX) {
-          printf("Merkkijonoon ei mahdu enempää merkkejä.");
-        } else {
-          bOnkoMjonossaTietuetta = TRUE; 
-          iRivi++;
+            printf("\n");
+            break;
+
+          case 2:
+            if (bOnkoMjonossaTietuetta == TRUE) {
+              for (int i = 0; i <= mjonokoko; i++) {
+                sMjono[i] = ' ';
+              }
+              sMjono[0] = '\0';
+              printf("\n");
+              printf("Merkkijono on tyhjennetty.\n");
+              bOnkoMjonossaTietuetta = FALSE; 
+            } else
+              printf("\nMerkkijono on tyhjä.\n");
+
+            printf("\n");
+            break;
+
+          case 3:
+            if (bOnkoMjonossaTietuetta == TRUE) {
+              printf("\n");
+              printf("Merkkijono: %s", sMjono);
+              printf("\n");
+            } else
+              printf("\nMerkkijono on tyhjä.\n");
+
+            printf("\n");
+            break;
+
+          default:
+            printf("\n");
+            printf("Tuntematon valinta.\n");
+            printf("\n");
+            break;
         }
+      } while(nValinta != 0);
 
-        /*getchar();*/
-        printf("\n");
-        break;
+      printf("\nOhjelman lopetus.\n");
 
-      case 2:
-        if (bOnkoMjonossaTietuetta == TRUE) {
-          for (int i = 0; i <= mjonokoko; i++) {
-            sMjono[i] = ' ';
-          }
-          printf("\n");
-          printf("Merkkijono on tyhjennetty.\n");
-          bOnkoMjonossaTietuetta = FALSE; 
-        } else
-          printf("\nMerkkijono on tyhjä.\n");
-
-        printf("\n");
-        break;
-
-      case 3:
-        if (bOnkoMjonossaTietuetta == TRUE) {
-          printf("\n");
-          printf("Merkkijono: ");
-          for (int i = 0; i < mjonokoko; i++) {
-            printf("%c", sMjono[i]);
-          }
-          printf("\n");
-        } else
-          printf("\nMerkkijono on tyhjä.\n");
-
-        printf("\n");
-        break;
-
-      default:
-        printf("\n");
-        printf("Tuntematon valinta.\n");
-        printf("\n");
-        break;
+      return 0;
     }
-  } while(nValinta != 0);
 
-  printf("\nOhjelman lopetus.\n");
-
-  return 0;
-}
-
-int annaKoko( char *str) {
-  int koko = 0;
-  while (str[koko] != '\0') {
-    koko++;
-  }
-  return koko;
-}
+    int annaKoko( char *str) {
+      int iRivi = 0;
+      for (iRivi = 0; str[iRivi] != '\0';iRivi++) {
+      }
+      return iRivi;
+    }
 
 
-/******************************************************************/
-/* eof */ 
+    /******************************************************************/
+    /* eof */ 
+
+
